a100f3a857f73f8acc460247dfdb6ae40ac46866
Dāvis Šķipsna <skipsna.davis@gmail.com>
2020-09-28 17:42:49 +0300

    Modified routes and renamed add pug to new pug

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,29 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.redirect("/books");
+});
+
+router.get("/books", (req, res) => {
   res.render("index");
 });
 
-router.get("/create_book", (req, res) => {
-  res.render("add");
+router.get("/books/new", (req, res) => {
+  res.render("new");
 });
 
-router.get("/book", (req, res) => {
+router.post("/books/new", (req, res) => {
+  res.redirect("/books");
+});
+
+router.get("/books/:id", (req, res) => {
   res.render("book");
 });
+
+router.post("/books/:id", (req, res) => {
+  res.redirect("/books");
+});
+
+router.post("/books/:id/delete", (req, res) => {});
 
 module.exports = router;
