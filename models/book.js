b5d04352f09499defd36d808cba6aa2a1b23e40e
Dāvis Šķipsna <skipsna.davis@gmail.com>
2020-09-28 23:04:29 +0300

    Created book model

--- models/book.js
+++ models/book.js
@@ -1,2 +1,36 @@
+const Sequelize = require("sequelize");
 
-
+module.exports = (sequelize) => {
+  class Book extends Sequelize.Model {}
+  Book.init({
+    title: {
+      type: Sequelize.STRING,
+      validate: {
+        notNull: {
+          msg: "Please provide book title",
+        },
+        notEmpty: {
+          msg: "Please provide book title",
+        },
+      },
+    },
+    author: {
+      type: Sequelize.STRING,
+      validate: {
+        notNull: {
+          msg: "Please provide book author",
+        },
+        notEmpty: {
+          msg: "Please provide book author",
+        },
+      },
+    },
+    genre: {
+      type: Sequelize.STRING,
+    },
+    year: {
+      type: Sequelize.NUMBER,
+    },
+  });
+  return Book;
+};
