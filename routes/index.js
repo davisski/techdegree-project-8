74fd1bc1b5941bd3fe47b68090e62bb7703ebaaf
Dāvis Šķipsna <skipsna.davis@gmail.com>
2020-09-28 17:09:08 +0300

    Adding routes and created some view templates

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/create_book", (req, res) => {
+  res.render("add");
+});
+
+router.get("/book", (req, res) => {
+  res.render("book");
+});
+
 module.exports = router;
